1330d1f99a8fa1305c1628d46ae144cee71ef6b0
o6ez9na <kikikippplo@gmail.com>
2024-11-19 14:39:14 +0300

    no more create all on startup

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
 from contextlib import asynccontextmanager
-from models import Base, db_helper
 from routes.routes import router as user_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 app = FastAPI(lifespan=lifespan)
