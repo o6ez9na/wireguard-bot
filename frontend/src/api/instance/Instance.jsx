d729a49fb7a31c54b78d19a9a1569e277a201fef
o6ez9na <kikikippplo@gmail.com>
2024-11-21 14:22:11 +0300

    rm

--- frontend/src/api/instance/Instance.jsx
+++ frontend/src/api/instance/Instance.jsx
@@ -4,7 +4,6 @@ const Instance = axios.create({
   baseURL: "http://localhost:8000/api/v1/",
   timeout: 5000,
   headers: {
-    // "Content-Type": "application/json",
     "Content-Type": "application/x-www-form-urlencoded",
   },
   withCredentials: true, // To enable cookies
