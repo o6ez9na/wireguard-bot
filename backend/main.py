d679db4c40be2be6db2a7dfa418eae2e6d0b834f
o6ez9na <kikikippplo@gmail.com>
2024-11-21 13:39:14 +0300

    add debug

--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,21 @@ from fastapi import FastAPI
 import uvicorn
 from contextlib import asynccontextmanager
 from routes.routes import router as user_router, admin_router
+from starlette.middleware.cors import CORSMiddleware
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(debug=True)
 app.include_router(user_router)
 app.include_router(admin_router)
-
+app.add_middleware(
+    CORSMiddleware,
+    # Разрешает доступ с любых доменов (например, из браузера на другом порту)
+    allow_origins=["*"],
+    allow_credentials=True,
+    # Разрешает все HTTP-методы (GET, POST, PUT, DELETE и т.д.)
+    allow_methods=["*"],
+    allow_headers=["*"],  # Разрешает все заголовки
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
