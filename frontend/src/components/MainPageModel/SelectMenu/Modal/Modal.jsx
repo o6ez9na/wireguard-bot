06dd23b57c61a322049ad875e544971c577478d9
o6ez9na <kikikippplo@gmail.com>
2024-11-22 09:53:34 +0300

    add animation for modal

--- frontend/src/components/MainPageModel/SelectMenu/Modal/Modal.jsx
+++ frontend/src/components/MainPageModel/SelectMenu/Modal/Modal.jsx
@@ -1,13 +1,28 @@
-import React from "react";
-import "./modal.css"; // Добавьте CSS для модального окна
+import React, { useEffect, useState } from "react";
+import "./modal.css";
 
 const Modal = ({ title, message, onClose }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    setShowModal(true);
+  }, []);
+
+  // Закрытие модального окна
+  const handleClose = () => {
+    setShowModal(false);
+    setTimeout(onClose, 300);
+  };
+
   return (
-    <div className="modal-overlay" onClick={onClose}>
+    <div
+      className={`modal-overlay ${showModal ? "show" : ""}`}
+      onClick={handleClose}
+    >
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <h3>{title}</h3>
         <p>{message}</p>
-        <button onClick={onClose}>Close</button>
+        <button onClick={handleClose}>Close</button>
       </div>
     </div>
   );
