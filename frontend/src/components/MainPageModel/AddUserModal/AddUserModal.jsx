e678942f3150ced7437b56ee11d6e04dd0e1255c
o6ez9na <kikikippplo@gmail.com>
2024-11-22 15:04:58 +0300

    add animations

--- frontend/src/components/MainPageModel/AddUserModal/AddUserModal.jsx
+++ frontend/src/components/MainPageModel/AddUserModal/AddUserModal.jsx
@@ -5,20 +5,29 @@ import "./add_modal.css";
 export const AddUserModal = ({ onClose }) => {
   const [isVisible, setIsVisible] = useState(false);
 
+  // Плавное появление модального окна
   useEffect(() => {
-    setIsVisible(true); // Модальное окно появляется с анимацией
+    setIsVisible(true);
 
     return () => {
-      setIsVisible(false); // Закрываем модальное окно с анимацией
+      // Плавное исчезновение
+      setIsVisible(false);
     };
   }, []);
+
+  const handleClose = () => {
+    setIsVisible(false); // Запускаем процесс исчезновения
+    setTimeout(() => {
+      onClose(); // Закрываем окно только после завершения анимации
+    }, 300); // 300 ms – время, соответствующее продолжительности анимации
+  };
 
   return (
     <div className={`add-modal-overlay ${isVisible ? "show" : ""}`}>
       <div className="add-modal-content">
         <h3>Модальное окно</h3>
         <p>Это пример модального окна.</p>
-        <button onClick={onClose}>Закрыть</button>
+        <button onClick={handleClose}>Закрыть</button>
       </div>
     </div>
   );
