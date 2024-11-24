18af01185e5fa0c37a840229a89c224f590a6e42
o6ez9na <kikikippplo@gmail.com>
2024-11-24 22:25:43 +0300

    refactoring

--- frontend/src/components/MainPageModel/MainPage.jsx
+++ frontend/src/components/MainPageModel/MainPage.jsx
@@ -2,52 +2,19 @@ import React, { useState } from "react";
 import "./main.css";
 import UserTable from "./UserTable/UserTable";
 import UsersInfo from "./UsersInfo/UsersInfo";
-import { AddUserModal } from "./AddUserModal/AddUserModal";
 
 export default function MainPage() {
-  const [isModalOpen, setIsModalOpen] = useState(false); // Состояние для отображения модального окна
-  const [shouldUpdateTable, setShouldUpdateTable] = useState(false); // Состояние для обновления таблицы
-
   // Открытие модального окна
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
 
   // Закрытие модального окна
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
 
   // Функция для обновления данных в таблице
-  const updateTable = () => {
-    setShouldUpdateTable((prev) => !prev); // Изменяем состояние для триггера перерисовки
-  };
 
   return (
     <div className="main-page-wrapper">
       <UsersInfo />
       <div className="main-pannel-with-users">
-        <div className="user-icons">
-          <div className="main-icon-user" onClick={openModal}>
-            <img
-              className="table-icon"
-              src={process.env.PUBLIC_URL + "/auth-icons/plus.svg"}
-              alt="plus"
-            />
-          </div>
-          <div className="main-icon-user">
-            <img
-              className="table-icon"
-              src={process.env.PUBLIC_URL + "/auth-icons/refresh.svg"}
-              alt="refresh"
-            />
-          </div>
-        </div>
-        <hr className="main-hr"></hr>
-        <UserTable shouldUpdate={shouldUpdateTable} />
-        {isModalOpen && (
-          <AddUserModal onClose={closeModal} onUserAdded={updateTable} />
-        )}
+        <UserTable />
       </div>
     </div>
   );
--- frontend/src/components/MainPageModel/UserTable/UserTable.jsx
+++ frontend/src/components/MainPageModel/UserTable/UserTable.jsx
@@ -2,27 +2,45 @@ import React, { useState, useEffect } from "react";
 import Switch from "../Switch/Switch";
 import Instance from "../../../api/instance/Instance";
 import SelectMenu from "../SelectMenu/SelectMenu";
+import { AddUserModal } from "../AddUserModal/AddUserModal";
 
-export default function UserTable({ shouldUpdate }) {
+export default function UserTable() {
   const [data, setData] = useState([]);
   const [openMenuId, setOpenMenuId] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isTableVisible, setIsTableVisible] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false); // Состояние для отображения модального окна
+  const [shouldUpdate, setShouldUpdateTable] = useState(false); // Состояние для обновления таблицы
 
-  // Функция для загрузки данных из базы
   const fetchData = async () => {
     try {
+      setIsTableVisible(false);
+      setIsLoading(true);
       const response = await Instance.get("/client");
       setData(response.data);
+      setIsLoading(false);
+      setTimeout(() => setIsTableVisible(true), 100);
     } catch (error) {
       console.error("Ошибка при загрузке данных:", error);
     }
   };
 
-  // Загружаем данные при монтировании компонента и при изменении shouldUpdate
   useEffect(() => {
     fetchData();
-  }, [shouldUpdate]); // fetchData будет вызываться при каждом изменении shouldUpdate
+  }, [shouldUpdate]);
 
-  // Обновление состояния в родительском компоненте при изменении статуса переключателя
+  const updateTable = () => {
+    setShouldUpdateTable((prev) => !prev); // Изменяем состояние для триггера перерисовки
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
   const handleSwitchChange = (user, newStatus) => {
     const updatedData = data.map((row) =>
       row.id === user.id ? { ...row, is_active: newStatus } : row
@@ -38,49 +56,82 @@ export default function UserTable({ shouldUpdate }) {
   };
 
   return (
-    <div className="table-wrapper">
-      <table className="user-table-wrapper">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Menu</th>
-            <th>Enabled</th>
-            <th>Name</th>
-            <th>Private key</th>
-            <th>Public key</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row) => (
-            <tr key={row.id}>
-              <td>{row.id}</td>
-              <td>
-                <SelectMenu
-                  id={row.id}
-                  openMenuId={openMenuId}
-                  setOpenMenuId={setOpenMenuId}
-                  onDelete={handleDeleteUser}
-                />
-              </td>
+    <>
+      <div className="user-icons">
+        <div className="main-icon-user" onClick={openModal}>
+          <img
+            className="table-icon"
+            src={process.env.PUBLIC_URL + "/auth-icons/plus.svg"}
+            alt="plus"
+          />
+        </div>
+        <div className="main-icon-user" onClick={fetchData}>
+          <img
+            className="table-icon"
+            src={process.env.PUBLIC_URL + "/auth-icons/refresh.svg"}
+            alt="refresh"
+          />
+        </div>
+      </div>
+      <hr className="main-hr"></hr>
 
-              <td>
-                <Switch
-                  user={row} // Передаем весь объект пользователя
-                  checked={row.is_active} // Убедитесь, что это свойство верно
-                  onChange={(user, newStatus) =>
-                    handleSwitchChange(user, newStatus)
-                  }
-                />
-              </td>
-              <td>{row.name}</td>
-              <td>{row.public_key}</td>
-              <td>{row.private_key}</td>
-              <td>{row.description}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+      <div className="table-wrapper">
+        {!isLoading ? (
+          <table
+            className={`user-table-wrapper ${
+              isTableVisible ? "visible-table" : ""
+            }`}
+          >
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Menu</th>
+                <th>Enabled</th>
+                <th>Name</th>
+                <th>Private key</th>
+                <th>Public key</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((row) => (
+                <tr key={row.id}>
+                  <td>{row.id}</td>
+                  <td>
+                    <SelectMenu
+                      id={row.id}
+                      openMenuId={openMenuId}
+                      setOpenMenuId={setOpenMenuId}
+                      onDelete={handleDeleteUser}
+                    />
+                  </td>
+
+                  <td>
+                    <Switch
+                      user={row} // Передаем весь объект пользователя
+                      checked={row.is_active} // Убедитесь, что это свойство верно
+                      onChange={(user, newStatus) =>
+                        handleSwitchChange(user, newStatus)
+                      }
+                    />
+                  </td>
+                  <td>{row.name}</td>
+                  <td>{row.public_key}</td>
+                  <td>{row.private_key}</td>
+                  <td>{row.description}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div className="loading-spinner-wrapper">
+            <div className="loading-spinner"></div>
+          </div>
+        )}
+      </div>
+      {isModalOpen && (
+        <AddUserModal onClose={closeModal} onUserAdded={updateTable} />
+      )}
+    </>
   );
 }
